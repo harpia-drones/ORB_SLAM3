9bf8cfa6e459e15824c9affe662c870c9396b6b1
Thiago <thiagooliveira1547@gmail.com>
2025-09-06 22:59:43 -0300

    chore: inital commit

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Adaptação do pacote ORB_SLAM
 
-### Requisitos
-- Pangolin == v0.6
+### Tested on
+- Ubuntu 24.04 LTS amd and arm
+- Pangolin == v0.6 (included as a thirdparty lib)
 - Eigen == 3.4.0
 - OpenCV >= 4.2.0
 
+## Installation
+
+```bash
+git clone git@github.com:harpia-drones/ORB_SLAM3.git
+cd ORB_SLAM3
+sudo bash build.sh
+```
---
+++ build.sh
@@ -0,0 +1,36 @@
+ORBSLAM3_ROOT_DIR="$(pwd)"
+
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-dev libglew-dev libeigen3-dev
+
+cd "${ORBSLAM3_ROOT_DIR}/Thirdparty/DBoW2"
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+
+cd "${ORBSLAM3_ROOT_DIR}/Thirdparty/g2o"
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+
+cd "${ORBSLAM3_ROOT_DIR}/Thirdparty/Sophus"
+mkdir -p build
+cd build
+cmake ..
+sudo make install
+
+cd "${ORBSLAM3_ROOT_DIR}/Thirdparty/Pangolin"
+mkdir -p build && cd build
+cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+make
+sudo make install
+
+cd "${ORBSLAM3_ROOT_DIR}"
+mkdir -p build && cd build
+cmake .. -DPANGOLIN_BUILD_SHARED=ON
+make
+sudo make install
